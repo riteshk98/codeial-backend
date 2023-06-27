e58ef11534b395e48e386b575d70d0e24a05c9d5
Ritesh Keswani <ritesh@Riteshs-MacBook-Air.local>
2023-06-27 21:22:25 +0530

    Create Directory Structure

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+app.listen(port, function(){
+    if(err){
+        console.log(`Error - ${err}`);
+    }
+    console.log(`Server is running on port - ${port}`);
+});
