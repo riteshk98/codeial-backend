b8fb28b698eb898aa7c548367446f34d18582046
Ritesh Keswani <ritesh@Riteshs-MacBook-Air.local>
2023-06-28 10:54:02 +0530

    Setup static files

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
-
 router.get('/', homeController.home);
-console.log('router loaded');
+router.use('/users', require('./users'));
+
 module.exports = router;
